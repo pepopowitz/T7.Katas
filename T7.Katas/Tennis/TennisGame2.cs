946f92d6cfb058e3d7822a72a09be4fa82d185ae
Steven Hicks <steven.hicks@centare.com>
2016-04-26 15:10:05 -0500

    refactor tennis game v2

--- T7.Katas/Tennis/TennisGame2.cs
+++ T7.Katas/Tennis/TennisGame2.cs
@@ -8,11 +8,9 @@ namespace T7.Katas.Tennis
 {
     public class TennisGame2 : ITennisGame
     {
-        public int P1point = 0;
-        public int P2point = 0;
+        public int player1Points;
+        public int player2Points;
 
-        public string P1res = "";
-        public string P2res = "";
         private string player1Name;
         private string player2Name;
 
@@ -22,130 +20,98 @@ namespace T7.Katas.Tennis
             this.player2Name = player2Name;
         }
 
-        public string GetScore()
+        public void WonPoint(string player)
         {
-            string score = "";
-            if (P1point == P2point && P1point < 3)
-            {
-                if (P1point == 0)
-                    score = "Love";
-                if (P1point == 1)
-                    score = "Fifteen";
-                if (P1point == 2)
-                    score = "Thirty";
-                score += "-All";
-            }
-            if (P1point == P2point && P1point > 2)
-                score = "Deuce";
-
-            if (P1point > 0 && P2point == 0)
-            {
-                if (P1point == 1)
-                    P1res = "Fifteen";
-                if (P1point == 2)
-                    P1res = "Thirty";
-                if (P1point == 3)
-                    P1res = "Forty";
-
-                P2res = "Love";
-                score = P1res + "-" + P2res;
-            }
-            if (P2point > 0 && P1point == 0)
-            {
-                if (P2point == 1)
-                    P2res = "Fifteen";
-                if (P2point == 2)
-                    P2res = "Thirty";
-                if (P2point == 3)
-                    P2res = "Forty";
-
-                P1res = "Love";
-                score = P1res + "-" + P2res;
-            }
-
-            if (P1point > P2point && P1point < 4)
-            {
-                if (P1point == 2)
-                    P1res = "Thirty";
-                if (P1point == 3)
-                    P1res = "Forty";
-                if (P2point == 1)
-                    P2res = "Fifteen";
-                if (P2point == 2)
-                    P2res = "Thirty";
-                score = P1res + "-" + P2res;
-            }
-            if (P2point > P1point && P2point < 4)
-            {
-                if (P2point == 2)
-                    P2res = "Thirty";
-                if (P2point == 3)
-                    P2res = "Forty";
-                if (P1point == 1)
-                    P1res = "Fifteen";
-                if (P1point == 2)
-                    P1res = "Thirty";
-                score = P1res + "-" + P2res;
-            }
-
-            if (P1point > P2point && P2point >= 3)
-            {
-                score = "Advantage player1";
-            }
-
-            if (P2point > P1point && P1point >= 3)
-            {
-                score = "Advantage player2";
-            }
-
-            if (P1point >= 4 && P2point >= 0 && (P1point - P2point) >= 2)
-            {
-                score = "Win for player1";
-            }
-            if (P2point >= 4 && P1point >= 0 && (P2point - P1point) >= 2)
-            {
-                score = "Win for player2";
-            }
-            return score;
-        }
-
-        public void SetP1Score(int number)
-        {
-
-            for (int i = 0; i < number; i++)
+            if (player == this.player1Name)
             {
                 P1Score();
             }
-
-        }
-
-        public void SetP2Score(int number)
-        {
-
-            for (int i = 0; i < number; i++)
+            else
             {
                 P2Score();
             }
-
         }
 
         public void P1Score()
         {
-            P1point++;
+            player1Points++;
         }
 
         public void P2Score()
         {
-            P2point++;
+            player2Points++;
         }
 
-        public void WonPoint(string player)
+        public string GetScore()
         {
-            if (player == "player1")
-                P1Score();
-            else
-                P2Score();
+            return GetScoreIfTie()
+                ?? GetScoreIfWin()
+                ?? GetScoreIfAdvantage()
+                ?? GetScoreIfRegulation();
         }
 
+        private string GetScoreIfTie()
+        {
+            if (player1Points != player2Points)
+            {
+                return null;
+            }
+
+            if (player1Points < 3)
+            {
+                var score = _scoreLookup[player1Points];
+                return score + "-All";
+            }
+
+            return "Deuce";
+        }
+
+        private string GetScoreIfWin()
+        {
+            var pointDiff = Math.Abs(player1Points - player2Points);
+            var pointMax = Math.Max(player1Points, player2Points);
+            if (pointDiff < 2 || pointMax < 4)
+            {
+                return null;
+            }
+
+            return "Win for " + GetLeadingPlayer();
+        }
+
+        private string GetScoreIfAdvantage()
+        {
+            if (Math.Min(player1Points, player2Points) < 3)
+            {
+                return null;
+            }
+
+            return "Advantage " + GetLeadingPlayer();
+        }
+
+        private string GetScoreIfRegulation()
+        {
+            if (player1Points >= 4 || player2Points >= 4)
+            {
+                //shouldn't ever get here because the previous handlers cover this scenario, but just in case of refactoring...
+                return null;
+            }
+
+            return _scoreLookup[player1Points] + "-" + _scoreLookup[player2Points];
+        }
+
+        private string GetLeadingPlayer()
+        {
+            return player1Points > player2Points
+                ? player1Name
+                : player2Name;
+        }
+
+        private Dictionary<int, string> _scoreLookup = new Dictionary<int, string>()
+        {
+            {0, "Love" },
+            {1, "Fifteen" },
+            {2, "Thirty" },
+            {3, "Forty" }
+        };
     }
 }
