ea881ac310dac2eac44a0215d789ef5842beb549
Steven Hicks <steven.hicks@centare.com>
2016-04-26 10:58:28 -0500

    setup tennis kata

---
+++ T7.Katas/Tennis/ITennisGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T7.Katas.Tennis
+{
+    public interface ITennisGame
+    {
+        void WonPoint(string playerName);
+        string GetScore();
+
+    }
+}
